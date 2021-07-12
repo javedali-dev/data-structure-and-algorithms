5cd40b5abdae218968be22703de6e07459cc983c
Javedali0563 <javedali0563@gmail.com>
2021-07-12 21:42:19 +0530

    :blush: initial commit

---
+++ src/LinkedList/ReverseLinkedList/LinkedList.java
@@ -0,0 +1,74 @@
+package LinkedList.ReverseLinkedList;
+
+
+public class LinkedList {
+
+    class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void insertAtHead(Node head,int data){
+        Node node = new Node(data);
+        node.next = head;
+        head=node;
+    }
+
+    Node insertAtTail(Node head, int data){
+        Node node = new Node(data);
+        node.next = null;
+        Node temp  = head;
+        if(temp==null)
+        {
+            head=node;
+            return head;
+        }
+        while(temp.next!=null){
+            temp= temp.next;
+        }
+        temp.next = node;
+        return head;
+
+    }
+    void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp= temp.next;
+        }
+    }
+    void reverseNode(Node head){
+        Node temp = head;
+        while(temp.next!=null){
+            if((temp.data)%2==0){
+                Node newHead = null;
+                while(((temp.data%2)==0)&&(temp.next!=null)){
+                    insertAtHead(newHead,temp.data);
+                    temp=temp.next;
+                }
+                if((temp.data)%2==0){
+                    insertAtHead(newHead,temp.data);
+                }
+                display(newHead);
+            }
+            else{
+                System.out.println(temp.data+" ");
+                temp=temp.next;
+            }
+
+        }
+        if(temp.next==null && temp.data%2!=0){
+            System.out.println(temp.data+" ");
+        }
+
+    }
+
+
+
+
+}
---
+++ src/LinkedList/ReverseLinkedList/Tester.java
@@ -0,0 +1,18 @@
+package LinkedList.ReverseLinkedList;
+
+import java.util.Scanner;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        LinkedList linkedList = new LinkedList();
+        LinkedList.Node head = null;
+        for (int i = 0; i <= n; i++) {
+            int num = scanner.nextInt();
+            head=linkedList.insertAtTail(head,num);
+        }
+        linkedList.display(head);
+    }
+}
