5d781e2a4561bddc0eac21d5d72a5b629a287a4b
Javedali0563 <javedali0563@gmail.com>
2021-08-11 22:34:05 +0530

    :blush: count digits added

--- src/BasicProgramming/BasicofImplementation/CountDigits.java
+++ src/BasicProgramming/BasicofImplementation/CountDigits.java
@@ -1,4 +1,21 @@
 package BasicProgramming.BasicofImplementation;
 
-public class BasicMain {
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine();
+        char[] digits = a.toCharArray();
+        int[] arr = new int[10];
+        for(char x:digits){
+
+            int temp = Character.getNumericValue(x);
+            arr[temp]++;
+
+        }
+        for(int i=0;i<10;i++){
+            System.out.println(i+" "+arr[i]);
+        }
+    }
 }
