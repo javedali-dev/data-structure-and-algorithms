0131a591c204c4bb94fa178df015227403877ec6
Javedali0563 <javedali0563@gmail.com>
2021-08-13 20:58:48 +0530

    :zap: refactor code

--- src/BasicProgramming/GoingToOffice/GoingToOfficeMain.java
+++ src/BasicProgramming/GoingToOffice/GoingToOfficeMain.java
@@ -1,2 +1,26 @@
-package BasicProgramming.GoingToOffice;public class GoingToOfficeMain {
+package BasicProgramming.GoingToOffice;
+
+import java.util.Scanner;
+
+public class GoingToOfficeMain {
+    public static void main(String args[] ) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        long d = scan.nextLong();
+        long oc = scan.nextLong();
+        long of = scan.nextLong();
+        long od = scan.nextLong();
+        long cs = scan.nextLong();
+        long cb = scan.nextLong();
+        long cm = scan.nextLong();
+        long cd = scan.nextLong();
+
+
+        if(oc + (d-of)*od<=cb+((d/cs) * cm) +(d*cd)){
+            System.out.println("Online Taxi");
+        }
+        else{
+            System.out.println("Classic Taxi");
+        }
+    }
 }
