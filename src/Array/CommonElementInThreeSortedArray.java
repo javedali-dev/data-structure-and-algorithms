28c9c2e65ab827f4dabd305251c35b88c372be7b
Javedali0563 <javedali0563@gmail.com>
2021-08-19 12:51:20 +0530

    :zap: CountElementTreeSortedArray added

--- src/Array/CommonElementInThreeSortedArray.java
+++ src/Array/CommonElementInThreeSortedArray.java
@@ -1,4 +1,30 @@
 package Array;
 
+import java.util.*;
+
 public class CommonElementInThreeSortedArray {
+    static ArrayList<Integer> commonElement(int[] A, int[] B, int[] C, int n1, int n2, int n3){
+        TreeSet<Integer> set1 = new TreeSet<Integer>();
+        TreeSet<Integer> set2 = new TreeSet<Integer>();
+        TreeSet<Integer> set3 = new TreeSet<Integer>();
+        for (int i = 0; i <n1 ; i++) {
+            set1.add(A[i]);
+        }
+        for (int i = 0; i <n2 ; i++) {
+            set2.add(B[i]);
+        }
+        for (int i = 0; i <n3 ; i++) {
+            set3.add(C[i]);
+        }
+        set1.retainAll(set2);
+        set1.retainAll(set3);
+        return new ArrayList<Integer>(set1);
+    }
+
+    public static void main(String[] args) {
+        int[] A = {1, 5, 10, 20, 40, 80};
+        int[] B = {6, 7, 20, 80, 100};
+        int[] C = {3, 4, 15, 20, 30, 70, 80, 120};
+        Display.printArray(commonElement(A,B,C,A.length,B.length,C.length));
+    }
 }
--- src/Array/Display.java
+++ src/Array/Display.java
@@ -1,9 +1,16 @@
 package Array;
 
+import java.util.ArrayList;
+
 public class Display {
     public static void printArray(int[] arr){
         for (int i = 0; i <arr.length ; i++) {
             System.out.print(arr[i]+" ");
+        }
+    }
+    public static void printArray(ArrayList<Integer> arr){
+        for (int i = 0; i <arr.size() ; i++) {
+            System.out.print(arr.get(i)+" ");
         }
     }
 }
